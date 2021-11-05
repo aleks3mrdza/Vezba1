08bf3a5bcbfc4cb9e59d855c2cd62fea968299a7
aleks3mrdza <aleks3mrdza@hotmail.com>
2021-11-05 17:23:22 +0100

    Committed task array and list

--- Vezba/Program.cs
+++ Vezba/Program.cs
@@ -17,21 +17,38 @@ namespace Vezba
             c = 3;
             d = 4;
             e = 5;
-            
+
+            if (a > b)
+            {
+                Console.WriteLine("a je vece od b");
+            }
+            else if (a == b)
+            {
+                Console.WriteLine("a je jednako b");
+            }
+            else
+            {
+                Console.WriteLine("a nije vece od b");
+            }
+
             bool res3 = a > b;
             Console.WriteLine(res3);
 
             bool res4 = a == b;
             Console.WriteLine(res4);
-            
+
             bool res5 = true;
             res5 = res3 || res4;
             Console.WriteLine(res5);
 
+            bool res6 = true;
+            res6 = res3 && res4;
+            Console.WriteLine(res6);
+
             int res = a = ++b;
             Console.WriteLine(res);
 
-            
+
 
             string s1, s2, s3, s4, s5;
             s1 = "aleks1";
@@ -62,14 +79,14 @@ namespace Vezba
             //SABIRANJE
             int resault = a3 + a4 + a5;
             Console.WriteLine(resault);
-            //ODUYIMANJE
-            int resault1 = a1 + a2;
+            //ODUZIMANJE
+            int resault1 = a1 - a2;
             Console.WriteLine(resault1);
             //MNOZENJE
             int resault2 = a1 * a1 * a2 * a4;
             Console.WriteLine(resault2);
             //Deljenje
-            int resault3 = a4 * a4;
+            int resault3 = a4 / a4;
             Console.WriteLine(resault3);
 
             int increase = a1++;
@@ -86,19 +103,64 @@ namespace Vezba
             string s41 = "adsd";
             string s51 = "ad";
 
-            char c11 = c11 = 'a';
-            char c22 =c22 = '*';
-            char c33 = c33 = '1';
-            char c44 = c44 = '\"';
-            char c55 = c55 = '\n';
+            char c11 = 'a';
+            char c22 = '*';
+            char c33 = '1';
+            char c44 = '\"';
+            char c55 = '\n';
 
-            bool b11 = b11 = true;
-            bool b22 = b22 = true;
-            bool b33 = b33 = false;
-            bool b44 = b44 = true;
-            bool b55 = b55 = true;
+            bool b11 = true;
+            bool b22 = true;
+            bool b33 = false;
+            bool b44 = true;
+            bool b55 = true;
 
-             
+            int[] intiger5 = new int[5];
+            int[] intiger51 = new int[5] { 3, 2, 1, 2, 3 };
+            int[] intiger52 = new int[5] { 4, 5, 7, 6, -5 };
+            bool res8 = intiger52[2] > intiger52[1];
+            Console.WriteLine(res8);
+
+            /*4. Kreiraj niz stringova veličine 9;
+            Na prvu poziciju u nizu postavi vrednost "abcd"
+            Na devetu poziciju u nizu postavi vrednost "abcd 9"*/
+            string[] string1 = new string[9];
+            string1[0] = "abcd";
+            string1[8] = "abcd 9";
+            //Isčitaj vrednost petog elementa u nizu i smesti u novu promenljivu
+            string s = string1[4];
+            Console.WriteLine(s);
+
+            //1. Kreiraj listu integera
+            List<int> list1 = new List<int>();
+
+            //2. Kreiraj listu integera i inicijalizuj listu
+            List<int> list2 = new List<int>{1, 2, 3, 2, 1};
+
+            //3. Dodaj 5 integera u listu, možeš dodati proizvoljne vrednost
+            List<int> list3 = new List<int>();
+            list3.Add(2);
+            list3.Add(1);
+            list3.Add(3);
+            list3.Add(1);
+            list3.Add(2);
+
+            //4. Kreiraj listu stringova i inicijalizuj je
+            List<string> string11 = new List<string>();
+
+            //5. Dodaj u listu sledeće stringove "abvc", "dgdf", "htfh", "dfhf"
+            string11.Add("abvc");
+            string11.Add("dgdf");
+            string11.Add("htfh");
+            string11.Add("dfhf");
+
+            //6. Izbaci iz liste string "dgdf"
+            string11.Remove("dgdf");
+
+            //Bonus 7. Izbaci iz liste element sa index-om 0
+            string11.Remove(string11[0]);
+
+            //
         }
     }
 }
